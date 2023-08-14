275b527f802318667dab2dad79ff3bd3e8ec9f0d
FoPPi <mishk908@gmail.com>
2023-08-14 15:58:45 +0300

    0.1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundColor: {
+        custom: 'linear-gradient(180deg, #FD792F 0%, #FF5C00 100%)',
+      }
     },
   },
   plugins: [],
